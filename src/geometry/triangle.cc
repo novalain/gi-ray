3195408f11cac86a0d6a84a234fecf53eb0b531f
Carlbaum <oskarcarlbaum@gmail.com>
2016-09-15 23:56:39 +0200

    fixup! Removed Vertex Class, added commons & improved triangle clas

--- src/geometry/triangle.cc
+++ src/geometry/triangle.cc
@@ -2,14 +2,14 @@
 #include <iostream>
 
 Triangle::Triangle(Vertex v1, Vertex v2, Vertex v3) : v1_(v1), v2_(v2), v3_(v3) {
-    color_ = COLOR_WHITE;
-    CalcNormal();
+  color_ = COLOR_WHITE;
+  CalcNormal();
 }
 
 Triangle::Triangle(Vertex v1, Vertex v2, Vertex v3, glm::vec3 color) : v1_(v1), v2_(v2), v3_(v3), color_(color) {
-    CalcNormal();
+  CalcNormal();
 }
 
 void Triangle::CalcNormal() {
-    normal_ = glm::cross(v2_-v1_,v3_-v2_);
+  normal_ = glm::cross(v2_-v1_,v3_-v2_);
 }
