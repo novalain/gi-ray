79eff882c2a4b0ac7b1120325bf024a00a4b3437
Carlbaum <oskarcarlbaum@gmail.com>
2016-09-17 02:02:06 +0200

    Simple z-check implemented plus some extra commons! fix #39 (#42)
    * Simple z-check implemented plus some extra commons! fix #39
    
    * fixup! Simple z-check implemented plus some extra commons! fix #39
    
    * fixup! fixup! Simple z-check implemented plus some extra commons! fix #39

--- include/commons.h
+++ include/commons.h
@@ -1,10 +1,13 @@
 #include <glm/glm.hpp>
 
-#define COLOR_WHITE glm::vec3(255,255,255)
-#define COLOR_BLACK glm::vec3(  0,  0,  0)
-#define COLOR_RED   glm::vec3(255,  0,  0)
-#define COLOR_GREEN glm::vec3(  0,255,  0)
-#define COLOR_BLUE  glm::vec3(  0,  0,255)
+#define COLOR_WHITE   glm::vec3(255,255,255)
+#define COLOR_BLACK   glm::vec3(  0,  0,  0)
+#define COLOR_RED     glm::vec3(255,  0,  0)
+#define COLOR_GREEN   glm::vec3(  0,255,  0)
+#define COLOR_BLUE    glm::vec3(  0,  0,255)
+#define COLOR_CYAN    glm::vec3(  0,255,255)
+#define COLOR_MAGENTA glm::vec3(255,  0,255)
+#define COLOR_YELLOW  glm::vec3(255,255,  0)
 
 typedef glm::vec3 Vertex;
 typedef glm::vec3 Direction;
