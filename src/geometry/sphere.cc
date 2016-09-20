097b9746df70c15ccfd2014bbdf94bb8cec09bc4
novalain <michael.f.noven@gmail.com>
2016-09-20 12:10:45 +0200

    Add trivial Sphere class

---
+++ src/geometry/sphere.cc
@@ -0,0 +1,8 @@
+#include "sphere.h"
+
+Sphere::Sphere(Vertex position, float radius, ColorDbl color)
+    : radius_(radius) {}
+
+bool Sphere::RayIntersection(Ray& ray, float& z) {
+  return true;
+}
