7f96b90e6aedeea9b92806ff00f19889e3de6c02
novalain <michael.f.noven@gmail.com>
2016-09-24 11:23:23 +0200

    Create class IntersectionPoint

---
+++ include/intersection_point.h
@@ -0,0 +1,20 @@
+#ifndef INTERSECTION_POINT_H
+#define INTERSECTION_POINT_H
+
+#include "commons.h"
+#include "material.h"
+
+class IntersectionPoint {
+private:
+  Vertex position_;
+  Direction normal_;
+  Material material_;
+public:
+  IntersectionPoint(Vertex position, Direction normal, Material material_);
+
+  Vertex get_position() { return position_; }
+  Direction get_normal() { return normal_; }
+  Material get_material() { return material_; }
+};
+
+#endif //INTERSECTION_POINT_H
