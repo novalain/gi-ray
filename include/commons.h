6988ce8ae8116e40e89aa137a5d2c711ec726d6c
Carlbaum <oskarcarlbaum@gmail.com>
2016-09-30 20:12:47 +0200

    Added Light class and material_ to triangle and sphere classes. fix #77

--- include/commons.h
+++ include/commons.h
@@ -1,15 +1,18 @@
 #include <glm/glm.hpp>
-
-#define COLOR_WHITE   glm::vec3(255,255,255)
-#define COLOR_BLACK   glm::vec3(  0,  0,  0)
-#define COLOR_RED     glm::vec3(255,  0,  0)
-#define COLOR_GREEN   glm::vec3(  0,255,  0)
-#define COLOR_BLUE    glm::vec3(  0,  0,255)
-#define COLOR_CYAN    glm::vec3(  0,255,255)
-#define COLOR_MAGENTA glm::vec3(255,  0,255)
-#define COLOR_YELLOW  glm::vec3(255,255,  0)
-#define COLOR_PINK    glm::vec3(235, 16,255)
+#include "material.h"
 
 typedef glm::vec3 Vertex;
 typedef glm::vec3 Direction;
 typedef glm::vec3 ColorDbl;
+
+#define COLOR_WHITE   ColorDbl(255,255,255)
+#define COLOR_BLACK   ColorDbl(  0,  0,  0)
+#define COLOR_RED     ColorDbl(255,  0,  0)
+#define COLOR_GREEN   ColorDbl(  0,255,  0)
+#define COLOR_BLUE    ColorDbl(  0,  0,255)
+#define COLOR_CYAN    ColorDbl(  0,255,255)
+#define COLOR_MAGENTA ColorDbl(255,  0,255)
+#define COLOR_YELLOW  ColorDbl(255,255,  0)
+#define COLOR_PINK    ColorDbl(235, 16,255)
+
+#define PERFECT_MIRROR Material(0,1,0,COLOR_WHITE)
